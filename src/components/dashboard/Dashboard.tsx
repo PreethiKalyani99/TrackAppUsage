9f979c8724b4394e62f198fdb88da2fd32785813
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-01-06 17:50:58 +0530

    add dashboard component

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,28 @@
+import CategorySummary from "../summary/CategorySummary"
+import SelectCategory from "../select/SelectCategory"
+import Alert from "../common/alert/Alert"
+import { useLimit } from "../../hooks/useLimit"
+import { useData } from "../../hooks/useData"
+import "../../App.css"
+
+
+export default function Dashboard() {
+    const { data } = useData()
+    const { isAlertVisible, statusMessage, setIsAlertVisible } = useLimit()
+
+    return (
+        <>
+            <div className="container">
+                <CategorySummary data={data} />
+                <SelectCategory/>
+            </div>
+            {isAlertVisible &&
+                <Alert
+                    type={statusMessage.status.toLowerCase() === "success" ? "success" : "error"}
+                    message={statusMessage.reason}
+                    onClose={() => setIsAlertVisible(false)}
+                />
+            }
+        </>
+    )
+}
